269e081c75ce0a21b4482944d2ec9750745b8b72
bolanebyla <alexleskov2000@gmail.com>
2020-02-16 21:26:17 +0800

    Немного изменил список запрашиваемой информации

--- Application/DB.py
+++ Application/DB.py
@@ -4,7 +4,7 @@ def TC_list():
 
 
 def FC_list(TC_name):
-    if 'запрос от DB':
+    if not'запрос от DB':
         FC = {'TC_name': TC_name, 'FC': ['3-этаж', '2-этаж']}
     else:
         FC = {'TC_name': TC_name, 'FC': None}
@@ -12,16 +12,19 @@ def FC_list(TC_name):
 
 
 def rest_list(TC_name, FC=None):
-    restaurants = {'TC_name': TC_name, 'FC': FC, 'rests': [{'rest_id': 12345678, 'name': 'Шавуха у Петрухи'},
+    restaurants = {'TC_name': TC_name, 'FC': FC, 'rests': [{'rest_id': 12345678, 'name': 'Шавуха от Петрухи'},
                                                            {'rest_id': 10101010, 'name': 'KFC'}]}
     return restaurants
 
 
-def categories_rest(id_rest):
-    categories = {'rest_id': id_rest, 'categories': ['Шаурма', 'Напитки']}
+def categories_rest(rest_id):
+    categories = {'rest_name': 'Шавуха от Петрухи', 'TC_name': 'Сильвер молл', 'FC': None, 'rest_id': rest_id,
+                  'categories': ['Шаурма', 'Напитки']}
     return categories
 
 
 def menu_rest(rest_id, category):
-    menu = {'rest_id': rest_id, 'menu': [{'name': '', 'img': '', 'composition': '', 'price': ''}]}
+
+    menu = {'name_rest': 'Шавуха у Петрухи', 'rest_id': rest_id,
+            'menu': [{'name': '', 'img': '', 'composition': '', 'price': ''}]}
     return menu
