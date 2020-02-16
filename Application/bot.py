df36f584ed880d6e1a0a4db9c644950afb2f20e1
bolanebyla <alexleskov2000@gmail.com>
2020-02-16 21:25:28 +0800

    Добавил список категорий в меню бота

--- Application/bot.py
+++ Application/bot.py
@@ -8,6 +8,8 @@ from config import TOKEN
 
 bot = telebot.TeleBot(TOKEN)
 
+print(str(None))
+
 
 @bot.message_handler(commands=['start'])
 def start_message(message: Message):
@@ -17,12 +19,14 @@ def start_message(message: Message):
                      reply_markup=makeKeyboard_TC(DB.TC_list()),
                      parse_mode='HTML')
 
+
 # Список торговых центров (меню)
 def makeKeyboard_TC(TC):
     markup = types.InlineKeyboardMarkup()
     for i in TC:
         markup.add(types.InlineKeyboardButton(text=i['name'], callback_data='{"TC":"' + i['name'] + '"}'))
     return markup
+
 
 # Список фудкортов (меню)
 def makeKeyboard_FC(FC):
@@ -35,7 +39,9 @@ def makeKeyboard_FC(FC):
                                               callback_data='{"FC":{"name":"' + name + '","TC_name":"' + TC_name + '"}}'))
     markup.add(types.InlineKeyboardButton(text='<',
                                           callback_data='start'))
+
     return markup
+
 
 # Список рестаранов (меню)
 def makeKeyboard_rest(restaurants):
@@ -44,13 +50,39 @@ def makeKeyboard_rest(restaurants):
     markup = types.InlineKeyboardMarkup()
     for rest in restaurants['rests']:
         markup.add(
-            types.InlineKeyboardButton(text=rest['name'], callback_data='{"rest_id:"' + str(rest["rest_id"]) + '"}'))
+            types.InlineKeyboardButton(text=rest['name'], callback_data='{"rest_id":' + str(
+                rest["rest_id"]) + '}'))
 
     if FC:
         markup.add(types.InlineKeyboardButton(text='<', callback_data='{"TC":"' + TC_name + '"}'))
 
     else:
         markup.add(types.InlineKeyboardButton(text='<', callback_data='start'))
+        return markup
+    markup.add(types.InlineKeyboardButton(text='<<',
+                                          callback_data='start'))
+    return markup
+
+
+# Список категорий ресторана
+def makeKeyboard_categories(categories):
+    rest_id = str(categories['rest_id'])
+    TC_name = categories['TC_name']
+    FC = categories['FC']
+    print(FC)
+
+    markup = types.InlineKeyboardMarkup()
+    for name in categories['categories']:
+        markup.add(types.InlineKeyboardButton(text=name,
+                                              callback_data='{"cat":{"cat":"' + name + '","rest_id":"' + rest_id + '"}}'))
+    if FC:
+        markup.add(types.InlineKeyboardButton(text='<',
+                                              callback_data='{"FC":{"name":"' + FC + '","TC_name":"' + TC_name + '"}}'))
+    else:
+        markup.add(types.InlineKeyboardButton(text='<',
+                                              callback_data='{"TC":"' + TC_name + '"}'))
+    markup.add(types.InlineKeyboardButton(text='<<',
+                                          callback_data='start'))
     return markup
 
 
@@ -64,31 +96,52 @@ def handle_query(message):
     if data == 'start':
         bot.edit_message_text(chat_id=chat_id,
                               message_id=message_id,
-                              text='Привет!',
+                              text='Список торговых центров',
                               reply_markup=makeKeyboard_TC(DB.TC_list()),
                               parse_mode='HTML')
     # Список фудкортов
     elif data.startswith('{"TC"'):
         TC_name = json.loads(data)['TC']
+
         bot.edit_message_text(chat_id=chat_id,
                               message_id=message_id,
-                              text='Yes',
+                              text=TC_name + '\nСписок фудкортов',
                               reply_markup=makeKeyboard_FC(DB.FC_list(TC_name)),
                               parse_mode='HTML')
     # Список ресторанов
     elif data.startswith('{"FC"'):
-        print('тут')
         data = json.loads(data)
         TC_name = data['FC']['TC_name']
         FC_name = data['FC']['name']
+        if FC_name:
+            txt = f'{TC_name}\n{FC_name}\n'
+        else:
+            txt = f'{TC_name}\n'
         bot.edit_message_text(chat_id=chat_id,
                               message_id=message_id,
-                              text='Yes',
+                              text=txt + 'Список ресторанов',
                               reply_markup=makeKeyboard_rest(DB.rest_list(TC_name=TC_name, FC=FC_name)),
                               parse_mode='HTML')
-    # Меню ресторана
+
+    # Ктегории ресторана
     elif data.startswith('{"rest_id"'):
-        rest_id = json.loads(message.data)['rest_id']
+        data = json.loads(data)
+        rest_id = data['rest_id']
+        categories = DB.categories_rest(rest_id=rest_id)
+        FC = categories["FC"]
+        if FC:
+            txt = f'{categories["TC_name"]}\n{FC}\n{categories["rest_name"]}\n'
+        else:
+            txt = f'{categories["TC_name"]}\n{categories["rest_name"]}\n'
+        bot.edit_message_text(chat_id=chat_id,
+                              message_id=message_id,
+                              text=txt + 'Категории',
+                              reply_markup=makeKeyboard_categories(categories),
+                              parse_mode='HTML')
+
+    # Меню ресторана
+    # elif data.startswith():
+    #     rest_id = json.loads(message.data)['rest_id']
 
 
 print('Бот запущен')
