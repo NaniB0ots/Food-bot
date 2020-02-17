610289c189b49fd3c6b76c5b803398b585b1ac07
bolanebyla <alexleskov2000@gmail.com>
2020-02-17 21:15:31 +0800

    Добавил информацию в menu_rest()

--- Application/DB.py
+++ Application/DB.py
@@ -4,7 +4,7 @@ def TC_list():
 
 
 def FC_list(TC_name):
-    if not'запрос от DB':
+    if 'запрос от DB':
         FC = {'TC_name': TC_name, 'FC': ['3-этаж', '2-этаж']}
     else:
         FC = {'TC_name': TC_name, 'FC': None}
@@ -18,13 +18,20 @@ def rest_list(TC_name, FC=None):
 
 
 def categories_rest(rest_id):
-    categories = {'rest_name': 'Шавуха от Петрухи', 'TC_name': 'Сильвер молл', 'FC': None, 'rest_id': rest_id,
+    categories = {'rest_name': 'Шавуха от Петрухи', 'TC_name': 'Сильвер молл', 'FC': '2-этаж', 'rest_id': rest_id,
                   'categories': ['Шаурма', 'Напитки']}
     return categories
 
 
 def menu_rest(rest_id, category):
+    # Например
+    rest_name = 'Шавуха от Петрухи'
 
-    menu = {'name_rest': 'Шавуха у Петрухи', 'rest_id': rest_id,
-            'menu': [{'name': '', 'img': '', 'composition': '', 'price': ''}]}
+    path = f'images/{rest_name}/{category}/'
+    menu = {'rest_name': 'Шавуха от Петрухи', 'rest_id': rest_id, 'category': category,
+            'menu': [{'name': 'Шаурма большая',
+                      'img': path + 'Shavuha_Petr2(Shaurma)_Shaurma_midle.jpg',
+                      'composition': '', 'price': '150'},
+                     {'name': 'Шашлык', 'img': 'images/Шавуха от петрухи/Шаурма/cipa_chachlik_chicken.png',
+                      'composition': '', 'price': '310'}]}
     return menu
