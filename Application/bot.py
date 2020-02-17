c2b8b58c1a5c53f040dec8d785fbe3aeeb9fd805
bolanebyla <alexleskov2000@gmail.com>
2020-02-17 21:14:57 +0800

    Сделал вывод списка товаров, написал call_back информацию
    Нужно:
    1. Добавить обработчик
    2. Исправить кнопку назад (сначала удалять сообщение, а потом создавать новое) - добавить флаг в call_back

--- Application/bot.py
+++ Application/bot.py
@@ -74,7 +74,7 @@ def makeKeyboard_categories(categories):
     markup = types.InlineKeyboardMarkup()
     for name in categories['categories']:
         markup.add(types.InlineKeyboardButton(text=name,
-                                              callback_data='{"cat":{"cat":"' + name + '","rest_id":"' + rest_id + '"}}'))
+                                              callback_data='{"cat":{"cat":"' + name + '","rest_id":' + rest_id + '}}'))
     if FC:
         markup.add(types.InlineKeyboardButton(text='<',
                                               callback_data='{"FC":{"name":"' + FC + '","TC_name":"' + TC_name + '"}}'))
@@ -85,6 +85,61 @@ def makeKeyboard_categories(categories):
                                           callback_data='start'))
     return markup
 
+
+def makeKeyboard_menu(menu):
+    markup = types.InlineKeyboardMarkup()
+    rest_id = menu['rest_id']
+    category = menu['category']
+    for m in menu['menu']:
+        markup.add(
+            types.InlineKeyboardButton(text=m['name'],
+                                       callback_data='{"menu":{"i":' + str(menu['menu'].index(m)) +
+                                                     ',"rest_id":' + str(rest_id) + ',"cat":"' + category + '"}}'))
+    return markup
+
+
+def makeKeyboard_food(menu, index, quantity=1):
+    markup = types.InlineKeyboardMarkup()
+    rest_id = str(menu['rest_id'])
+    category = menu['category']
+    price = menu['menu'][index]['price']
+
+    # {"f":{"i":' + str(i) + ',"rest_id":' + str(rest_id) + ',"c":"' + category + ',""a:"' + 'struck' + '"}}'
+
+    # -, количество, +
+    action = ['-' + str(quantity), '+' + str(quantity)]
+    markup.add(types.InlineKeyboardButton(text='-',
+                                          callback_data='{"food":[' + rest_id + ',"' + category + '",' + str(
+                                              index) + ',"' + action[0] + '"]}'),
+               types.InlineKeyboardButton(text=str(quantity),
+                                          callback_data='None'),
+               types.InlineKeyboardButton(text='+',
+                                          callback_data='{"food":[' + rest_id + ',"' + category + '",' + str(
+                                              index) + ',"' + action[1] + '"]}'))
+
+    # Состав , Итого
+    action = 'sruct'
+    markup.add(types.InlineKeyboardButton(text='Состав',
+                                          callback_data='{"food":[' + rest_id + ',"' + category + '",' + str(
+                                              index) + ',"' + action + '"]}'),
+               types.InlineKeyboardButton(text='Итого: ' + str(quantity * price) + ' руб',
+                                          callback_data='None'))
+
+    # Добавить в корзину
+    action = 'add' + str(quantity)
+    markup.add(types.InlineKeyboardButton(text='Добавить в корзину',
+                                          callback_data='{"food":[' + rest_id + ',"' + category + '",' + str(
+                                              index) + ',"' + action + '"]}'))
+
+    # Назад
+    # Добавить флаг удаления сообщения
+    markup.add(types.InlineKeyboardButton(text='Назад',
+                                          callback_data='{"cat":{"cat":"' + category + '","rest_id":' + rest_id + '}}'))
+
+    return markup
+
+
+# callback_data size max 60
 
 @bot.callback_query_handler(func=lambda call: True)
 def handle_query(message):
@@ -140,8 +195,32 @@ def handle_query(message):
                               parse_mode='HTML')
 
     # Меню ресторана
-    # elif data.startswith():
-    #     rest_id = json.loads(message.data)['rest_id']
+    elif data.startswith('{"cat"'):
+        data = json.loads(data)
+        rest_id = data['cat']['rest_id']
+        category = data['cat']['cat']
+        menu = DB.menu_rest(rest_id=rest_id, category=category)
+        rest_name = menu['rest_name']
+
+        bot.edit_message_text(chat_id=chat_id,
+                              message_id=message_id,
+                              text=rest_name + '\nМеню',
+                              reply_markup=makeKeyboard_menu(menu),
+                              parse_mode='HTML')
+
+    elif data.startswith('{"menu"'):
+        data = json.loads(data)
+        rest_id = data['menu']['rest_id']
+        category = data['menu']['cat']
+        index = data['menu']['i']
+
+        # Отправка изображния
+        menu = DB.menu_rest(rest_id=rest_id, category=category)
+        img = open(menu['menu'][index]['img'], 'rb')
+        bot.send_photo(chat_id=chat_id, photo=img, reply_markup=makeKeyboard_food(menu, index))
+
+        # Удаление старого сообщения
+        bot.delete_message(chat_id=chat_id, message_id=message_id)
 
 
 print('Бот запущен')
