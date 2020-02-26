609d6e060fe1258c7eff074c9ed38aee4df592f4
bolanebyla <56492378+bolanebyla@users.noreply.github.com>
2020-02-26 11:10:45 +0800

    Add files via upload

--- Application/config.py
+++ Application/config.py
@@ -1,2 +1,3 @@
 TOKEN = '941022031:AAFgRROWBsU2eY2BT77MH9HbsvPvB9Ccljs'  # Food-bot_test1 @Test1_Food_bot
-URL = ''
+URL = 'https://bolanebyla.pythonanywhere.com/'
+PROVIDER_TOKEN = '381764678:TEST:14257'
